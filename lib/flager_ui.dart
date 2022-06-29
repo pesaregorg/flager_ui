c5d7639f8025d180e76491534ebb1e39cc847a53
mohammad <pesaregorg@gmail.com>
2022-06-29 23:42:45 +0430

    update theme

--- lib/flager_ui.dart
+++ lib/flager_ui.dart
@@ -1,8 +1,17 @@
 library flager_ui;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
-part 'theme_helper.dart';
+
+part 'constants.dart';
+part 'colors.dart';
+part 'buttons.dart';
+part 'widgets/elevated_tag.dart';
+
+part 'text_fields.dart';
+
 
 class FlagerUi{
 
@@ -15,7 +24,7 @@ class FlagerUi{
     bool? useMaterial3
   }){
 
-    var themeColor = primaryColor ?? Colors.blue;
+    var themeColor = primaryColor ?? FColors.primaries[0];
     ThemeData themeData = ThemeData(
       useMaterial3: useMaterial3 ?? true,
       primaryColor: themeColor,
@@ -29,11 +38,14 @@ class FlagerUi{
         splashColor: themeColor.withAlpha(50),
         hintColor: themeData.hintColor.withAlpha(90),
         errorColor: Colors.red,
-        inputDecorationTheme: ThemeHelper.inputDecorationTheme(themeData),
+        textButtonTheme: FButtons.textButtonThemeData(),
+        elevatedButtonTheme: FButtons.elevatedButtonThemeData(primaryColor: themeColor),
+        outlinedButtonTheme: FButtons.outlinedButtonThemeData(primaryColor: themeColor),
+        inputDecorationTheme: FTextFields.inputDecorationTheme(themeData: themeData),
         switchTheme: themeData.switchTheme.copyWith(
           trackColor: MaterialStateColor.resolveWith((states){
             if (states.contains(MaterialState.selected)) {
-              return themeColor.shade400;
+              return themeColor;
             }
             return Colors.grey.withOpacity(0.48);
           }),
@@ -67,5 +79,12 @@ class FlagerUi{
 
 
 
+  static colors(){
+
+  }
 
 }
+
+
+
+
