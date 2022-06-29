0e70e7b9c63b856b11645eb993136c0eec7dbe8d
mohammad <pesaregorg@gmail.com>
2022-06-29 23:41:49 +0430

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+
+part of flager_ui;
+
+
+class FConstants {
+
+  static const double tagBorderRadiusAll = 8.0;
+  static const double buttonBorderRadiusAll = 16.0;
+  static const double buttonPaddingVertical = 16.0;
+  static const double buttonPaddingHorizontal = 8.0;
+}
