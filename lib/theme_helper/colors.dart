5cfa6be08efd275409295892edc76d46027a1381
mohammad <pesaregorg@gmail.com>
2022-07-09 14:37:30 +0430

    add material color

---
+++ lib/theme_helper/colors.dart
@@ -0,0 +1,333 @@
+part of flager_ui;
+
+class FColors {
+  // This class is not meant to be instantiated or extended; this constructor
+  // prevents instantiation and extension.
+  FColors._();
+
+
+
+  static const MaterialColor homeenger = MaterialColor(
+    _homeengerPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFBEFBFF),
+      100: Color(0xFF33E0EB),
+      200: Color(0xFF2CC7D1),
+      300: Color(0xFF24AEB7),
+      400: Color(0xFF229FA7),
+      500: Color(_homeengerPrimaryValue),
+      600: Color(0xFF197C82),
+      700: Color(0xFF115C60),
+      800: Color(0xFF0C4346),
+      900: Color(0xFF072A2C),
+    },
+  );
+  static const int _homeengerPrimaryValue = 0xFF1E8F96;
+
+
+
+
+  static const MaterialColor homeengerRed = MaterialColor(
+    _homeengerRedPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFAE4E4),
+      100: Color(0xFFFAB4B4),
+      200: Color(0xFFFA9C9C),
+      300: Color(0xFFF88484),
+      400: Color(0xFFF47070),
+      500: Color(_homeengerRedPrimaryValue),
+      600: Color(0xFFC74949),
+      700: Color(0xFFAA3E3E),
+      800: Color(0xFF883131),
+      900: Color(0xFF431717),
+    },
+  );
+  static const int _homeengerRedPrimaryValue = 0xFFF15959;
+
+
+
+
+
+  static const MaterialColor sky = MaterialColor(
+    _skyPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFE3F4FF),
+      100: Color(0xFFBBE2FF),
+      200: Color(0xFF8DD0FF),
+      300: Color(0xFF56BDFF),
+      400: Color(0xFF1DADFF),
+      500: Color(_skyPrimaryValue),
+      600: Color(0xFF008FFF),
+      700: Color(0xFF007BFF),
+      800: Color(0xFF1269EC),
+      900: Color(0xFF1F47CD),
+    },
+  );
+  static const int _skyPrimaryValue = 0xFF009EFF;
+
+
+
+
+
+  static const MaterialColor infrared = MaterialColor(
+    _infraredPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFDE8E5),
+      100: Color(0xFFFFC8BB),
+      200: Color(0xFFFFA48F),
+      300: Color(0xFFFF7E62),
+      400: Color(0xFFFF5E40),
+      500: Color(_infraredPrimaryValue),
+      600: Color(0xFFFF311B),
+      700: Color(0xFFF32916),
+      800: Color(0xFFE61F10),
+      900: Color(0xFFCD0004),
+    },
+  );
+  static const int _infraredPrimaryValue = 0xFFFF4B36;
+
+
+
+
+
+
+  static const MaterialColor leaf = MaterialColor(
+    _leafPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFE6F5E6),
+      100: Color(0xFFC2E6C1),
+      200: Color(0xFF9BD59A),
+      300: Color(0xFF70C671),
+      400: Color(0xFF4DB951),
+      500: Color(_leafPrimaryValue),
+      600: Color(0xFF169E26),
+      700: Color(0xFF008C1A),
+      800: Color(0xFF007B0C),
+      900: Color(0xFF005D00),
+    },
+  );
+  static const int _leafPrimaryValue = 0xFF22AD2F;
+
+
+
+
+
+
+  static const MaterialColor fire = MaterialColor(
+    _firePrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFDF3E0),
+      100: Color(0xFFFCDFB1),
+      200: Color(0xFFFACA7E),
+      300: Color(0xFFF8B549),
+      400: Color(0xFFF6A223),
+      500: Color(_firePrimaryValue),
+      600: Color(0xFFF18900),
+      700: Color(0xFFEB7A00),
+      800: Color(0xFFE56B00),
+      900: Color(0xFFDC5100),
+    },
+  );
+  static const int _firePrimaryValue = 0xFFF59300;
+
+
+
+
+
+  static const MaterialColor oil = MaterialColor(
+    _oilPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFDFDFD),
+      100: Color(0xFFF7F7F7),
+      200: Color(0xFFE5E5E5),
+      300: Color(0xFFCCCCCC),
+      400: Color(0xFFABABAB),
+      500: Color(_oilPrimaryValue),
+      600: Color(0xFF696969),
+      700: Color(0xFF525252),
+      800: Color(0xFF3B3B3B),
+      900: Color(0xFF242424),
+    },
+  );
+  static const int _oilPrimaryValue = 0xFF808080;
+
+
+
+
+
+  /*static const MaterialColor storm = MaterialColor(
+    _stormPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFCFDFD),
+      100: Color(0xFFF6F8F9),
+      200: Color(0xFFE0E7EB),
+      300: Color(0xFFC2CFD6),
+      400: Color(0xFF9AB0BC),
+      500: Color(_stormPrimaryValue),
+      600: Color(0xFF54707D),
+      700: Color(0xFF415762),
+      800: Color(0xFF2F3F46),
+      900: Color(0xFF1D262B),
+
+    },
+  );
+  static const int _stormPrimaryValue = 0xFF668899;*/
+
+
+
+
+  /*static const MaterialColor twilight = MaterialColor(
+    _twilightPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFF9FEFD),
+      100: Color(0xFFF2FCFC),
+      200: Color(0xFFD4F7F4),
+      300: Color(0xFFAAEEE9),
+      400: Color(0xFF72E3DA),
+      500: Color(_twilightPrimaryValue),
+      600: Color(0xFF23AFA3),
+      700: Color(0xFF1B887F),
+      800: Color(0xFF13625C),
+      900: Color(0xFF0C3C37),
+
+    },
+  );
+  static const int _twilightPrimaryValue = 0xFF2AD5C6;*/
+
+
+
+
+  /*static const MaterialColor ultraViolet = MaterialColor(
+    _ultraVioletPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFDFAFF),
+      100: Color(0xFFF7F0FE),
+      200: Color(0xFFE6CFFC),
+      300: Color(0xFFCC9EFA),
+      400: Color(0xFFAB5FF6),
+      500: Color(_ultraVioletPrimaryValue),
+      600: Color(0xFF680AC6),
+      700: Color(0xFF52089B),
+      800: Color(0xFF3B066F),
+      900: Color(0xFF240344),
+
+    },
+  );
+  static const int _ultraVioletPrimaryValue = 0xFF7F0DF1;*/
+
+
+
+
+
+
+  /*static const MaterialColor sun = MaterialColor(
+    _sunPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFFFEFA),
+      100: Color(0xFFFEFAEF),
+      200: Color(0xFFFBF0D0),
+      300: Color(0xFFF7E2A1),
+      400: Color(0xFFF2CF63),
+      500: Color(_sunPrimaryValue),
+      600: Color(0xFFC0940F),
+      700: Color(0xFF96740C),
+      800: Color(0xFF6C5309),
+      900: Color(0xFF423305),
+
+    },
+  );
+  static const int _sunPrimaryValue = 0xFFEBB513;*/
+
+
+
+
+  /*static const MaterialColor ocean = MaterialColor(
+    _oceanPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFF9FBFE),
+      100: Color(0xFFF3FAFC),
+      200: Color(0xFFD6F0F5),
+      300: Color(0xFFADE0EB),
+      400: Color(0xFF78CDDD),
+      500: Color(_oceanPrimaryValue),
+      600: Color(0xFF2A93A7),
+      700: Color(0xFF217283),
+      800: Color(0xFF17525E),
+      900: Color(0xFF0E3239),
+
+    },
+  );
+  static const int _oceanPrimaryValue = 0xFF33B3CC;*/
+
+
+
+
+/*  static const MaterialColor wood = MaterialColor(
+    _woodPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFEFBF9),
+      100: Color(0xFFFCF7F3),
+      200: Color(0xFFF5E3D6),
+      300: Color(0xFFEBC7AD),
+      400: Color(0xFFDDA278),
+      500: Color(_woodPrimaryValue),
+      600: Color(0xFFA75E2A),
+      700: Color(0xFF834A21),
+      800: Color(0xFF5E3417),
+      900: Color(0xFF39200E),
+
+    },
+  );
+  static const int _woodPrimaryValue = 0xFFCC7333;*/
+
+
+
+
+
+/*  static const MaterialColor soil = MaterialColor(
+    _soilPrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFDFCFB),
+      100: Color(0xFFFAF8F5),
+      200: Color(0xFFEEE7DD),
+      300: Color(0xFFDECFBA),
+      400: Color(0xFFC8B08D),
+      500: Color(_soilPrimaryValue),
+      600: Color(0xFF8D6F44),
+      700: Color(0xFF6E5635),
+      800: Color(0xFF4F3E26),
+      900: Color(0xFF302617),
+
+    },
+  );
+  static const int _soilPrimaryValue = 0xFFAC8753;*/
+
+
+
+
+  static const Color black = Color(0xFF000000);
+
+
+  static const Color white = Color(0xFFFFFFFF);
+
+
+
+
+
+  static const List<MaterialColor> primaries = <MaterialColor>[
+    homeenger,
+    sky,
+    infrared,
+    leaf,
+    fire,
+    oil,
+    //storm,
+    //twilight,
+    //ultraViolet,
+    //sun,
+    //ocean,
+    //wood,
+    //soil,
+  ];
+
+}
